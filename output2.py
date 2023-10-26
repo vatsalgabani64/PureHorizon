5cbc486a9827d68ac411860ded2339b3d4619684
Vatsal <gabanivatsal17@gmail.com>
2023-10-26 19:56:58 +0530

    Background and other minor changes

--- output2.py
+++ output2.py
@@ -25,13 +25,38 @@ def load_lottieurl(url):
 
 
 # Use local CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+# def local_css(file_name):
+#     with open(file_name) as f:
+#         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+# local_css("style.css")
+# st.markdown(
+#     f'<style>{open("style.css").read()}</style>',
+#     unsafe_allow_html=True,
+# )
 
-local_css("style.css")
+css = '''
+.stApp {
+    # background: black   ;
+    # background-size: cover;
+    # background-position: center;
+    # background-color: #333;
+    # color: white;
+    background: url('https://images.unsplash.com/photo-1487621167305-5d248087c724?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+}
+.stApp > header {
+    background-color: transparent;
+}
+a.link-class {
+    color: red; /* Change the desired link color here */
+}
+.st-my-custom-class a{
+    color: blue;
 
+}
+'''
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+# st.markdown(f'<style>{open("style.css").read()}</style>', unsafe_allow_html=True)
 # ---- LOAD ASSETS ----
 lottie_coding = load_lottieurl("https://lottie.host/a96988fe-9d6f-448b-94c1-97b710cd53bf/Sg5SAAAUOw.json")
 img_contact_form = Image.open("image_01.jpg")
@@ -41,9 +66,9 @@ img_lottie_animation = Image.open("image_01.jpg")
 st.title("PureHorizon")
 st.subheader("Air Pollution Prediction & Visualization")
 st.write("PureHorizon is a platform that uses machine learning to predict air quality and visualize pollution data. It can be used to track air quality in real time, identify pollution hotspots, and forecast air quality conditions. PureHorizon is a valuable tool for businesses, governments, and individuals who need to make informed decisions about air quality.")
-st.write("[More information about PureHorizon]()") # added link for more info of our project
-
-# ---- WHAT I DO ----
+# st.write("[More informatio  n about PureHorizon]()") # added link for more info of our project
+st.markdown("<div class='st-my-custom-class'><a href='https://purehorizon.streamlit.app'>More information about PureHorizon</a></div>", unsafe_allow_html=True)
+# ---- WHAT WE DO ----
 with st.container():
     st.write("---")
     left_column, right_column = st.columns([2,1])
@@ -57,7 +82,6 @@ with st.container():
             - Data Visualization for pollutants and AQI index
             """
         )
-        # st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
     with right_column:
         st.write('<style>div[data-widget="stLottie"] { background-color: lightblue; }</style>', unsafe_allow_html=True)
         
@@ -66,37 +90,10 @@ with st.container():
 # ---- PROJECTS ----
 with st.container():
     st.write("---")
-    st.header("Air Quality Analysis and Vizulization")
+    st.header("Air Quality Analysis and Visulization")
     st.write("##")
         
-# st.markdown(
-#     """
-#     <style>
-#     # *{
-#     #     background-color: #FF99FF;
-#     #     color: black;
-#     # }
-#     .st-eb {
-#         width: 50%;
-#     }
-#     table{
-#         border-collapse:collapse;
-#         width: 70%;
-#     }
-#     th, td {
-#         width: 50%;
-#     }
-#     div[data-widget="stOptionMenu"] .stOptionMenu {
-#         color:white;
-#         padding: 10px;
-#         margin: 0;
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
 
-# df = pd.read_csv('predicted.csv')
 standards = pd.read_csv('standard.csv')
 
 def Home_screen():
@@ -114,7 +111,6 @@ def Home_screen():
         """
     )
 
-
 def Analysis_screen():
     st.title('Predicted Pollutant Levels')
 
@@ -124,7 +120,7 @@ def Analysis_screen():
     st.markdown(
         """
         <style>
-        div[data-baseweb="select"]{
+        div[data-baseweb="select"] {
             font-size:25px;
         }
         </style>
@@ -135,6 +131,10 @@ def Analysis_screen():
     st.subheader("Select City")
     selected_city = st.selectbox(label='',options=city_options)
     df=pd.read_csv(f'predicted_{selected_city}.csv')
+
+    st.subheader("Select Pollutant")
+    pollutants = st.multiselect('', ['AQI','PM2.5','PM10','SO2','CO','Ozone','NO2'])
+
     if selected_city:
         highest_score_index = df['Future_Date'][df['prediction_AQI'].idxmax()]
         split_string = re.split(r'[:\s-]', highest_score_index)
@@ -143,13 +143,13 @@ def Analysis_screen():
         split_string = re.split(r'[:\s-]', highest_score_index)
         st.write(f"Lowest AQI obeserved : {split_string[3]}:{split_string[4]} on {split_string[2]}/{split_string[1]}")
 
-    st.subheader("Select Pollutant")
-    pollutants = st.multiselect('', ['AQI','PM2.5','PM10','SO2','CO','Ozone','NO2'])
+    
     if pollutants:
         df.set_index(df['Future_Date'],inplace=True)
-        # st.write(df[pollutants])
         lst = [f'prediction_{x}' for x in pollutants]
-        st.write(df[lst])
+        lst = ['Future_Date'] + lst[0:]
+        st.markdown(df[lst].style.hide(axis="index").to_html(), unsafe_allow_html=True)
+        # st.write(df[lst])
 
 def Visualization_screen():
     # Sample list of options
@@ -189,30 +189,6 @@ if selected == "Analysis":
 if selected == "Visualization":
     Visualization_screen()
 
-# ---- CONTACT ----
-# with st.container():
-#     st.write("---")
-#     st.header("Get In Touch With Me!")
-#     st.write("##")
-
-#     # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
-#     contact_form = """
-#     <form action="https://formsubmit.co/YOUR@MAIL.COM" method="POST">
-#         <input type="hidden" name="_captcha" value="false">
-#         <input type="text" name="name" placeholder="Your name" required>
-#         <input type="email" name="email" placeholder="Your email" required>
-#         <textarea name="message" placeholder="Your message here" required></textarea>
-#         <button type="submit">Send</button>
-#     </form>
-#     """
-#     left_column, right_column = st.columns(2)
-#     with left_column:
-#         st.markdown(contact_form, unsafe_allow_html=True)
-#     with right_column:
-#         st.empty()
-
-
-
 def configure():
     load_dotenv()
 
@@ -234,8 +210,8 @@ cursor.execute('''
 ''')
 
 # Streamlit app
-st.title("Contact Form")
-st.write("Contact PureHorizon for any queries or feedback.")
+st.title("Subscribe")
+st.write("Join in to get daily weather updates")
 with st.form("Contact Form"):
     city_options = ['Gandhinagar','Ahmedabad']
 
